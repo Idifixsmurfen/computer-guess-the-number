95cde9cf7a875fba10585e8b6a059c5d0e44bebd
Idifixsmurfen <62641006+Idifixsmurfen@users.noreply.github.com>
2020-11-09 15:57:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const MAX_NUMBER = 100
-const MIN_NUMBER = 0
+let MAX_NUMBER = 100
+let MIN_NUMBER = 0
 
 let guess
 
 document.getElementById('too-high-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too high?
+    //setMessage(`Is it ${guess-- -1}?`)
+    //guess = Math.floor (Math.random () * 101 )
 
+    guess = (MAX_NUMBER + MIN_NUMBER) / 2
+    setMessage(`Is it ${guess}?`)
+    guess = MAX_NUMBER
 })
 
 document.getElementById('too-low-btn').addEventListener('click', function () {
     // Vad ska hända när man tryckt too low?
+    //setMessage(`Is it ${guess++ +1}?`)
+    //guess = Math.floor (Math.random () * 101 )
     
+    guess = (MAX_NUMBER + MIN_NUMBER) / 2
+    setMessage(`Is it ${guess}?`)
+    guess = MIN_NUMBER
 })
 
+document.getElementById('is-correct-btn').addEventListener('click', function () {
+    // Vad ska hända när man tryckt Correct?
+    setMessage("Woho! :)")
+})
 
 function setMessage(msg) {
     document.getElementById('message').innerText = msg
@@ -22,9 +36,9 @@ function start() {
     document.getElementById('before-start').style.display = 'none'
     document.querySelector('main').style.display = 'block'
 
-    guess = 0
+    guess = 50
 
-    // setMessage(`Is it ${guess}?`)
+    setMessage(`Is it ${guess}?`)
 }
 
 document
